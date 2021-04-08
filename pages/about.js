635043b506189adb7483701ac2a8889e7f4ef28d
sachitbhat <sachitbhat@deloitte.com>
2021-04-08 12:53:35 -0500

    minor

--- pages/about.js
+++ pages/about.js
@@ -19,11 +19,11 @@ const about = () => {
           <h1 className={styles.h1}>Hello and Welcome! </h1>          
           <h3 className={styles.h2, styles.bodyText}>
         Before anything else, a quick note on my name. I usually introduce myself as 'Sachit, like Watch-it', so most people know me as that. My correct birth-name is actually pronounced a bit differently: 
-        the 'a' is pronounced like a 'uh' and the 't' is pronounced like 'th' (S-uh-chith). It's not entirely intuitive, but follows the same pattern as many other East Asian/Indian names. I have been recently been going by Sach (pronounced like 'Such').
+        the 'a' is pronounced 'uh' and the 't' is pronounced 'th' (S-uh-chith). It's not entirely intuitive, but follows the same pattern as many other East Asian/Indian names. I have been recently been going by Sach (pronounced 'Such').
 </h3>
 
 <h3 className={styles.h2, styles.bodyText}>
-The purpose of the this site is to simply consolidate pieces of life I enjoy and create a springboard for future ideas and experiences. I like to try new things, and built this site to exist as my home base.
+The purpose of the this site is to simply consolidate pieces of life I enjoy and create a springboard for future ideas and experiences. I like to try new things, and built this site to serve as my home base.
 </h3>
 <h3 className={styles.h2, styles.bodyText}>
 
@@ -33,10 +33,10 @@ In the past, I've worked as an engineer at <a className={styles.aboutLink} href=
 </h3>
 <h3 className={styles.h2, styles.bodyText}>
 
-Outside of tech, I thoroughly enjoy reading, writing, walking, board games, HIIT, and self-tracking. I challenged myself to write a <a className={styles.aboutLink} href="http://newsletter.sachitbhat.com/" target="_blank">newsletter</a> for a year during quarantine, both to track my own progress but also incentive curiosity and learning. More recently, I created a <a className={styles.aboutLink} href="https://coda.io/@sachit-bhat/sb" target="_blank">page</a> to store articles and podcasts as well as a quick blurb of my thoughts. I crave spice, and am slightly addicted to Cholula, eggs, and avocado (This has been my daily lunch and weekend brunch since going remote). My recent focus has been web development and prototyping. I used
+Outside of tech, I thoroughly enjoy reading, writing, walking, board games, HIIT, and self-tracking. I challenged myself to write a <a className={styles.aboutLink} href="http://newsletter.sachitbhat.com/" target="_blank">newsletter</a> for a year during quarantine, both to track my own progress and incentivize curiosity and learning. More recently, I created a <a className={styles.aboutLink} href="https://coda.io/@sachit-bhat/sb" target="_blank">page</a> to store articles and podcasts as well as a quick blurb of my thoughts. I crave spice, and am slightly addicted to Cholula, eggs, and avocado (This has been my daily lunch and weekend brunch since going remote). My recent focus has been web development and prototyping. I used
 Next.js to build this site and am continuing to experiment with <a className={styles.aboutLink} href="https://nextjs.org/Next" target="_blank">Next</a> and React.
 
-If you'd like to connect, you can message me <a href="mailto:sachitbhat.sb@gmail.com" className={styles.aboutLink} target="_blank">here</a>  or connect with me on <a className={styles.aboutLink} href="https://www.linkedin.com/in/sachitbhat/" target="_blank">LinkedIn</a>. Else, hope you enjoyed reading🙂    
+If you'd like to connect, feel free to message me <a href="mailto:sachitbhat.sb@gmail.com" className={styles.aboutLink} target="_blank">here</a>  or connect with me on <a className={styles.aboutLink} href="https://www.linkedin.com/in/sachitbhat/" target="_blank">LinkedIn</a>. Else, hope you enjoyed reading🙂    
           
           
           </h3>
