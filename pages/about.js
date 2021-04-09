1c3091dadd170dbca0dedefe202ce48dc0ff96b4
sachitbhat <sachitbhat@deloitte.com>
2021-04-09 10:51:00 -0500

    clean up

--- pages/about.js
+++ pages/about.js
@@ -1,56 +1,149 @@
-import Head from 'next/head'
-import React from 'react'
-import styles from '../styles/Home.module.css'
-import Link from 'next/link'
-import {ABOUT} from '../utils/constants.js'
+import Head from "next/head";
+import React from "react";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
+import { ABOUT } from "../utils/constants.js";
 
 const about = () => {
   return (
     <div className={styles.container}>
-    
-    <main className={styles.main}>
+      <main className={styles.main}>
         <Link href="/">
-          <a id='top' className={styles.topLink}>
+          <a id="top" className={styles.topLink}>
             Home
           </a>
         </Link>
-        <hr className={styles.headerLine}/>
+        <hr className={styles.headerLine} />
         <div className={styles.aboutText}>
-          <h1 className={styles.h1}>Hello and Welcome! </h1>          
-          <h3 className={styles.h2, styles.bodyText}>
-        Before anything else, a quick note on my name. I usually introduce myself as 'Sachit, like Watch-it', so most people know me by that that. My birth name is actually pronounced a bit differently: 
-        the 'a' is pronounced 'uh' and the 't' is pronounced 'th' (S-uh-chith). It's not entirely intuitive, but <a className={styles.aboutLink} href="https://www.youtube.com/watch?v=F__SBto-uYg&ab_channel=GreatVideos-GVCMH" target="_blank">follows the same pattern as many other East Asian/Indian names</a>. I have been recently been going by Sach (pronounced 'Such').
-</h3>
-
-<h3 className={styles.h2, styles.bodyText}>
-The purpose of the this site is to simply consolidate pieces of life I enjoy and create a springboard for future ideas and experiences. I like to try new things, and built this site to serve as my home base.
-</h3>
-<h3 className={styles.h2, styles.bodyText}>
-
-I have interests spanning industries, companies and concepts. I am a sucker for B2C/B digital startups, asymmetric risk, early-stage venture capital, and systems-thinking. I currently work as a software engineer in <a className={styles.aboutLink} href="https://www.deloittedigital.com/" target="_blank">Deloitte's Digital studio</a>, though continue to push myself to view each endeavor through the eyes of a product manager (including my own life). 
-
-In the past, I've worked as an engineer at <a className={styles.aboutLink} href="https://opennetworking.org/" target="_blank">ON.Lab</a>, a software networking consortium continuing to disrupt traditional network infrastructure. More recently, I was a fellow in the <a className={styles.aboutLink} href="https://vimeo.com/295238898" target="_blank">True Ventures Entrepreneurship Corps</a>, where I was paired with <a className={styles.aboutLink} href="https://www.airship.com/" target="_blank">Airship</a> to help with product engineering and analytics. I graduated from <a className={styles.aboutLink} href="https://www.vanderbilt.edu/" target="_blank">Vanderbilt University</a> in 2019, where I studied computer science and economics.
-</h3>
-<h3 className={styles.h2, styles.bodyText}>
-
-Outside of tech, I thoroughly enjoy reading, writing, walking, board games, HIIT, and self-tracking. I challenged myself to write a <a className={styles.aboutLink} href="http://newsletter.sachitbhat.com/" target="_blank">newsletter</a> for a year during quarantine, both to track my own progress and incentivize curiosity and learning. More recently, I created a <a className={styles.aboutLink} href="https://coda.io/@sachit-bhat/sb" target="_blank">page</a> to store my thoughts on relevant articles and podcasts. I crave spice, and am slightly addicted to Cholula, eggs, and avocado (This has been my daily lunch and weekend brunch since going remote). 
-I built this site using <a className={styles.aboutLink} href="https://nextjs.org/Next" target="_blank">Next.js</a> and React, yet continue to experiement with new technologies and frameworks.
-
-If you'd like to connect, feel free to message me <a href="mailto:sachitbhat.sb@gmail.com" className={styles.aboutLink} target="_blank">here</a>  or connect with me on <a className={styles.aboutLink} href="https://www.linkedin.com/in/sachitbhat/" target="_blank">LinkedIn</a>. Else, hope you enjoyed reading🙂    
-          
-          
+          <h1 className={styles.h1}>Hello and Welcome! </h1>
+          <h3 className={(styles.h2, styles.bodyText)}>
+            Before anything else, a quick note on my name. I usually introduce
+            myself as 'Sachit, like Watch-it', so most people know me by that
+            that. My birth name is actually pronounced a bit differently: the
+            'a' is pronounced 'uh' and the 't' is pronounced 'th' (S-uh-chith).
+            It's not entirely intuitive, but{" "}
+            <a
+              className={styles.aboutLink}
+              href="https://www.youtube.com/watch?v=F__SBto-uYg&ab_channel=GreatVideos-GVCMH"
+              target="_blank"
+            >
+              follows the same pattern as many other East Asian/Indian names
+            </a>
+            . I have been recently been going by Sach (pronounced 'Such').
           </h3>
 
-        </div>        
-        <hr className={styles.headerLine}/>
-        <div className={styles.toTop}>
-          <a href='#top'>Back to top</a>
+          <h3 className={(styles.h2, styles.bodyText)}>
+            The purpose of the this site is to simply consolidate pieces of life
+            I enjoy and create a springboard for future ideas and experiences. I
+            like to try new things, and built this site to serve as my home
+            base.
+          </h3>
+          <h3 className={(styles.h2, styles.bodyText)}>
+            I have interests spanning industries, companies and concepts. I am a
+            sucker for B2C/B digital startups, asymmetric risk, early-stage
+            venture capital, and systems-thinking. I currently work as a
+            software engineer in{" "}
+            <a
+              className={styles.aboutLink}
+              href="https://www.deloittedigital.com/"
+              target="_blank"
+            >
+              Deloitte's Digital studio
+            </a>
+            , though continue to push myself to view each endeavor through the
+            eyes of a product manager (including my own life). In the past, I've
+            worked as an engineer at{" "}
+            <a
+              className={styles.aboutLink}
+              href="https://opennetworking.org/"
+              target="_blank"
+            >
+              ON.Lab
+            </a>
+            , a software networking consortium continuing to disrupt traditional
+            network infrastructure. More recently, I was a fellow in the{" "}
+            <a
+              className={styles.aboutLink}
+              href="https://vimeo.com/295238898"
+              target="_blank"
+            >
+              True Ventures Entrepreneurship Corps
+            </a>
+            , where I was paired with{" "}
+            <a
+              className={styles.aboutLink}
+              href="https://www.airship.com/"
+              target="_blank"
+            >
+              Airship
+            </a>{" "}
+            to help with product engineering and analytics. I graduated from{" "}
+            <a
+              className={styles.aboutLink}
+              href="https://www.vanderbilt.edu/"
+              target="_blank"
+            >
+              Vanderbilt University
+            </a>{" "}
+            in 2019, where I studied computer science and economics.
+          </h3>
+          <h3 className={(styles.h2, styles.bodyText)}>
+            Outside of tech, I thoroughly enjoy reading, writing, walking, board
+            games, HIIT, and self-tracking. I challenged myself to write a{" "}
+            <a
+              className={styles.aboutLink}
+              href="http://newsletter.sachitbhat.com/"
+              target="_blank"
+            >
+              newsletter
+            </a>{" "}
+            for a year during quarantine, both to track my own progress and
+            incentivize curiosity and learning. More recently, I created a{" "}
+            <a
+              className={styles.aboutLink}
+              href="https://coda.io/@sachit-bhat/sb"
+              target="_blank"
+            >
+              page
+            </a>{" "}
+            to store my thoughts on relevant articles and podcasts. I crave
+            spice, and am slightly addicted to Cholula, eggs, and avocado (This
+            has been my daily lunch and weekend brunch since going remote). I
+            built this site using{" "}
+            <a
+              className={styles.aboutLink}
+              href="https://nextjs.org/Next"
+              target="_blank"
+            >
+              Next.js
+            </a>{" "}
+            and React, yet continue to experiement with new technologies and
+            frameworks. If you'd like to connect, feel free to message me{" "}
+            <a
+              href="mailto:sachitbhat.sb@gmail.com"
+              className={styles.aboutLink}
+              target="_blank"
+            >
+              here
+            </a>{" "}
+            or connect with me on{" "}
+            <a
+              className={styles.aboutLink}
+              href="https://www.linkedin.com/in/sachitbhat/"
+              target="_blank"
+            >
+              LinkedIn
+            </a>
+            . Else, hope you enjoyed reading🙂
+          </h3>
         </div>
-    </main>
+        <hr className={styles.headerLine} />
+        <div className={styles.toTop}>
+          <a href="#top">Back to top</a>
+        </div>
+      </main>
+    </div>
+  );
+};
 
-
-  </div>
-  )
-}
-
-export default about
+export default about;
--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,38 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import {NAME} from '../utils/constants.js'
-import {LINKS} from '../utils/constants.js'
-import { FaKey, FaSpotify, FaCamera, FaProductHunt, FaLinkedin, FaGoodreads, FaMedium, FaPencilAlt } from 'react-icons/fa';
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import { NAME } from "../utils/constants.js";
+import { LINKS } from "../utils/constants.js";
+import {
+  FaKey,
+  FaSpotify,
+  FaCamera,
+  FaProductHunt,
+  FaLinkedin,
+  FaGoodreads,
+  FaMedium,
+  FaPencilAlt,
+} from "react-icons/fa";
 import { RiArticleFill } from "react-icons/ri";
 import { BiArrowToBottom } from "react-icons/bi";
-import Tooltip from '@material-ui/core/Tooltip';
-import Link from 'next/link'
-import { motion } from "framer-motion"
-import {withStyles, makeStyles} from '@material-ui/core/styles'
-
+import Tooltip from "@material-ui/core/Tooltip";
+import Link from "next/link";
+import { motion } from "framer-motion";
+import { withStyles, makeStyles } from "@material-ui/core/styles";
 
 export default function Home() {
-  const icons1={
-    'FaLinkedin': FaLinkedin,
-    'FaPencilAlt': FaPencilAlt,
-    'RiArticleFill': RiArticleFill,
-    'BiArrowToBottom': BiArrowToBottom,
-    'FaCamera': FaCamera,
-    'FaSpotify':  FaSpotify,
-    'FaGoodreads': FaGoodreads,
-    'FaKey': FaKey,
-    'FaProductHunt': FaProductHunt
-  }
+  const icons1 = {
+    FaLinkedin: FaLinkedin,
+    FaPencilAlt: FaPencilAlt,
+    RiArticleFill: RiArticleFill,
+    BiArrowToBottom: BiArrowToBottom,
+    FaCamera: FaCamera,
+    FaSpotify: FaSpotify,
+    FaGoodreads: FaGoodreads,
+    FaKey: FaKey,
+    FaProductHunt: FaProductHunt,
+  };
   const data = LINKS;
-  
+
   return (
     <div className={styles.container}>
       <Head>
@@ -34,50 +42,47 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <Link href="/about">
-          <a className={styles.topLink}>
-            About
-          </a>
+          <a className={styles.topLink}>About</a>
         </Link>
-        <hr className={styles.headerLine}/>
-            <img src="/prof.png" alt="Sachit Bhat Profile" className={styles.logo} />
+        <hr className={styles.headerLine} />
+        <img
+          src="/prof.png"
+          alt="Sachit Bhat Profile"
+          className={styles.logo}
+        />
         <h1 className={styles.h1}>Sachit Bhat </h1>
-        <hr className={styles.line1}/>
+        <hr className={styles.line1} />
         <h2 className={styles.h2}>{NAME.subText}</h2>
-        <hr className={styles.headerLine}/>
-        { <div className = {styles.buttonList}>
+        <hr className={styles.headerLine} />
+        {
+          <div className={styles.buttonList}>
             {data.map((item, index) => {
-              console.log(item)
-              const Icon = icons1[item.icon]
+              console.log(item);
+              const Icon = icons1[item.icon];
               return (
                 <div>
-                <Tooltip title={item?.tooltip} >
-                  <div title={item.title} className={styles.button}>
-                    <a href={item.link} target='_blank'>
-                      <motion.div
-                      whileHover={{ scale: 1.3 }}
-                      whileTap={{ scale: 0.9 }}
-                      >
-                        <Icon key={index} size={30}/>
-                      </motion.div>
-                    </a>
-                  </div>
-                </Tooltip>
+                  <Tooltip title={item?.tooltip}>
+                    <div title={item.title} className={styles.button}>
+                      <a href={item.link} target="_blank">
+                        <motion.div
+                          whileHover={{ scale: 1.3 }}
+                          whileTap={{ scale: 0.9 }}
+                        >
+                          <Icon key={index} size={30} />
+                        </motion.div>
+                      </a>
+                    </div>
+                  </Tooltip>
                 </div>
-              )
+              );
             })}
-        </div>
-        
+          </div>
         }
-
-
-       
       </main>
 
       <footer className={styles.footer}>
-        <h2 className={styles.h2}>&copy; 2021 Sachit Bhat 
-
-        </h2>
+        <h2 className={styles.h2}>&copy; 2021 Sachit Bhat</h2>
       </footer>
     </div>
-  )
+  );
 }
